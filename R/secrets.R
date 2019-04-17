df8470422b30b98a2858267755ebb5403e6d8450
Hong Ooi <hongooi@microsoft.com>
2019-04-17 17:19:49 +1000

    secret doc

--- R/secrets.R
+++ R/secrets.R
@@ -1,3 +1,64 @@
+#' Stored secrets in Key Vault
+#'
+#' This class represents the collection of secrets stored in a vault. It provides methods for managing secrets, including creating, importing and deleting secrets, and doing backups and restores.
+#'
+#' @docType class
+#'
+#' @section Methods:
+#' This class provides the following methods:
+#' ```
+#' create(name, value, content_type=NULL, attributes=vault_object_attrs(), ...)
+#' get(name)
+#' delete(name, confirm=TRUE)
+#' list()
+#' backup(name)
+#' restore(backup)
+#' ```
+#' @section Arguments:
+#' - `name`: The name of the secret.
+#' - `value`: For `create`, the secret to store. This should be a character string or a raw vector.
+#' - `content_type`: For `create`, an optional content type of the secret, such as "application/octet-stream".
+#' - `attributes`: Optional attributes for the secret, such as the expiry date and activation date. A convenient way to provide this is via the [vault_object_attrs] helper function.
+#' - `...`: For `create`, other named arguments which will be treated as tags.
+#' - `backup`: For `restore`, a string representing the backup blob for a secret.
+#'
+#' @section Value:
+#' For `get`, and `create`, an object of class `stored_secret`, representing the secret. The actual value of the secret is in the `value` field.
+#'
+#' For `list`, a vector of secret names.
+#'
+#' For `backup`, a string representing the backup blob for a secret. If the secret has multiple versions, the blob will contain all versions.
+#'
+#' @seealso
+#' [vault_object_attrs]
+#'
+#' [Azure Key Vault documentation](https://docs.microsoft.com/en-us/azure/key-vault/),
+#' [Azure Key Vault API reference](https://docs.microsoft.com/en-us/rest/api/keyvault)
+#'
+#' @examples
+#' \dontrun{
+#'
+#' vault <- key_vault$new("mykeyvault")
+#'
+#' vault$secrets$create("mysecret", "secret string")
+#'
+#' vault$secrets$list()
+#'
+#' secret <- vault$secrets$get("mysecret")
+#' secret$value  # 'secret string'
+#'
+#' # specifying an expiry date
+#' today <- Sys.date()
+#' vault$secrets$create("mysecret", attributes=vault_object_attrs(expiry_date=today+365))
+#'
+#' # setting management tags
+#' vault$secrets$create("mysecret", tag1="a value", othertag="another value")
+#'
+#' }
+#' @name secrets
+#' @rdname secrets
+NULL
+
 vault_secrets <- R6::R6Class("vault_secrets", 
 
 public=list(
